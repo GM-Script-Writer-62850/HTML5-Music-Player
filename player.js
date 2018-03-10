64cbf6b6bb4da8838e71d82ab35bacd86020059c
GM-Script-Writer-62850 <pqwoerituytrueiwoq@gmail.com>
2018-03-09 20:07:30 -0500

    Add ID3, Upgrade CSS, & Bug Fixes

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 "use strict";
-var track,audio,pic,title,TITLE,shuffle,repeat,loop,err,playlist,offset,unPlayed,
-	cLog=false,// Show messages in brower console
+var track,audio,pic,title,ID3,shuffle,repeat,loop,err,playlist,offset,unPlayed,
+	log=false,// Show messages in brower console
 	music=Array(),
 	hst={
 		"log":[],
@@ -8,7 +8,7 @@ var track,audio,pic,title,TITLE,shuffle,repeat,loop,err,playlist,offset,unPlayed
 		"nav":false,
 		"add":function(){
 			var trim=hst.log.length;
-			log('Add track '+track+' to history');
+			log('Add track',track,'to history');
 			hst.log.push(track);
 			hst.log=hst.log.slice(music.length*-1);
 			if(hst.indx>hst.log.length)
@@ -19,32 +19,31 @@ var track,audio,pic,title,TITLE,shuffle,repeat,loop,err,playlist,offset,unPlayed
 function getId(id){
 	return document.getElementById(id);
 }
-function log(msg){
-	if(cLog)
-		console.log(msg);
-	return cLog;
-}
 function randInt(min,max){
 	max++;
 	var rand=Math.round(Math.random()*(max-min)+min);
-	return rand==max?min:rand;
+	rand=rand==max?min:rand;
+	log("RNG",{"min":min,"max":max-1,"int":rand});
+	return rand;
 }
 function sendEvt(element,event){
-	log('Event: '+event.substr(0,1).toUpperCase()+event.substr(1)+' on #'+element.id);
+	log('Send',event,'event to',element);
 	//element.dispatchEvent(new Event(event));//This does not work in IE11
 	var evt = document.createEvent("HTMLEvents");
 	evt.initEvent(event, true, true );
 	return !element.dispatchEvent(evt);
 }
 function extToMime(ext){
-	log('File Extension: '+ext);
+	ext=ext.toLowerCase();
+	log('File Extension:',ext);
 	switch(ext){
 		case "mp3":return "audio/mpeg";
 		case "ogg":return "audio/ogg";
 		case "aac":return "audio/aac";
 		case "wav":return "audio/wave";
 		case "webm":return "audio/webm";
-		default: "audio/"+ext;
+		case "flac":return "audio/flac";
+		default: return "audio/"+ext;
 	}
 }
 function wait4It(fn){// Dirty trick
@@ -72,13 +71,13 @@ function reloadUnPlayed(init){
 }
 function setPlayed(i,test){
 	if(test===false){
-		log('#'+i+' has been played');
+		log('Track',i,'has been played');
 		music[i].setAttribute('played','yes');
 		return;
 	}
 	i=unPlayed.indexOf(i);
 	if(unPlayed.length>0){
-		log('Tracks not played: '+unPlayed.length);
+		log('Tracks not played:',unPlayed.length);
 		if(audio.currentTime/audio.duration >= 0.15){// At least 15% of file was played
 			setPlayed(track,false);
 			if(i>-1){
@@ -90,20 +89,31 @@ function setPlayed(i,test){
 			}
 		}
 		else{
-			log('Tack #'+i+' was Skipped');
+			log('Tack',i,'was Skipped');
 			i++;
 		}
 	}
-	else{
+	i=i==unPlayed.length?0:i;
+	if(unPlayed.length==0){
 		reloadUnPlayed(false);
 	}
-	return i==unPlayed.length?0:i;
+	return i;
+}
+function applyID3(id3){
+	var unown='<Unknown>';
+/*	if(id3.title&&id3.title[0]){
+		title.head.textContent=id3.title+" | "+title.page;
+	}*/
+	ID3.title.textContent=id3.title&&id3.title[0]?id3.title[0]:unown;
+	ID3.artist.textContent=id3.artist&&id3.artist[0]?id3.artist[0]:unown;
+	ID3.album.textContent=id3.album&&id3.album[0]?id3.album[0]:unown;
+	ID3.year.textContent=id3.year&&id3.year[0]?id3.year[0]:unown;
 }
 function populateList(arr,e,dir){
 	var li,i,x,cover;
 	for(i in arr){
 		if(i!="/"){
-			log('Found Folder: '+i);
+			log('Found Folder:',i);
 			li=document.createElement('li');
 			li.className="folder";
 			li.textContent=i;
@@ -123,20 +133,23 @@ function populateList(arr,e,dir){
 		else{
 			cover=false;
 			for(x in arr[i]){
-				if(arr[i][x].slice(0,arr[i][x].lastIndexOf('.')).toLowerCase()=="cover"){
-					log('Found Cover: '+arr[i][x]);
-					cover=arr[i][x];
-					arr[i].splice(x,1);
+				if(!arr[i][x] && x.slice(0,x.lastIndexOf('.')).toLowerCase()=="cover"){
+					log('Found Cover:',x);
+					cover=x;
+					delete(arr[i][x]);
 					break;
 				}
 			}
 			for(x in arr[i]){
-				log('Found Audio: '+arr[i][x]);
+				log('Found Audio:',x);
 				li=document.createElement('li');
 				li.id=music.length;
 				li.className="song";
-				li.textContent=arr[i][x].substr(0,arr[i][x].lastIndexOf('.'));
-				li.setAttribute('file',dir+arr[i][x]);
+				li.textContent=x.substr(0,x.lastIndexOf('.'));//title
+				if(arr[i][x]){// id3 tags
+					li.setAttribute('id3',JSON.stringify(arr[i][x]));
+				}
+				li.setAttribute('file',dir+x);
 				li.setAttribute('cover',cover?dir+cover:'/icons/sound2.png');
 				li.addEventListener('click',function(event){
 					event.stopPropagation();
@@ -149,9 +162,10 @@ function populateList(arr,e,dir){
 					var file=this.getAttribute('file'),
 						cover=this.getAttribute('cover'),
 						s=document.createElement('source'),
-						mime=extToMime(file.substr(-3)),
-						last=music[track];
-					log("Now Playing: "+file);
+						mime=extToMime(file.substr(file.lastIndexOf('.')+1)),
+						last=music[track],
+						id3=this.getAttribute('id3');
+					log("Now Playing:",file);
 					if(last.className.indexOf('playing')>-1){
 						while(last.id!='playlist'){
 							last.className=last.className.slice(0,-8);
@@ -159,15 +173,16 @@ function populateList(arr,e,dir){
 						}
 					}
 					pic.src=cover;
-					title.textContent=this.textContent;
-					TITLE.textContent='Music Player: '+this.textContent;
-					if(!audio.canPlayType(mime)){
-						err.textContent="This browser does not support "+mime.substr(mime.indexOf('/')+1).toUpperCase()+" audio."
-						err.className='open';
+					title.player.textContent=this.textContent;
+					title.head.textContent=this.textContent+" | "+title.page;
+					if(id3!==null){
+						id3=JSON.parse(id3);
+						applyID3(id3);
 					}
-					else{
-						err.removeAttribute('class');
+					else if(library['id3']===true){
+						applyID3({});
 					}
+					showError(audio.canPlayType(mime)?false:"This browser does not support "+mime.substr(mime.indexOf('/')+1).toUpperCase()+" audio.");
 					s.type=mime;
 					s.src=file;
 					audio.appendChild(s);
@@ -182,6 +197,37 @@ function populateList(arr,e,dir){
 						last.className+=' playing';
 						last=last.parentNode.parentNode;
 					}
+					if(library['id3']===true&&id3===null){
+						var httpRequest=new XMLHttpRequest();
+						httpRequest.onreadystatechange=function(){
+							if(httpRequest.readyState==4){
+								if(httpRequest.status==200){
+									if(httpRequest.responseText.length==0){
+										return console.error("An undefined error occured while fetching ID3 data");
+									}
+									log("HTTP Request:",httpRequest.responseText);
+									var r=JSON.parse(httpRequest.responseText);
+									if(!r.id3){
+										return;
+									}
+									music[r.track].setAttribute('id3',JSON.stringify(r.id3));
+									if(track==r.track){
+										applyID3(r.id3);
+									}
+								}
+								else if(httpRequest.status==404){
+									showError("This Tack no longer exist!");
+									var r=JSON.parse(httpRequest.responseText);
+									log("404 Error:",music[r.track].getAttribute('file'));
+								}
+								else{
+									console.error("HTTP Status Code:"+httpRequest.status+"\n"+httpRequest.responseText);
+								}
+							}
+						};
+						httpRequest.open('GET','playlist.php?file='+encodeURIComponent(file)+'&track='+track);
+						httpRequest.send(null);
+					}
 				},false);
 				e.appendChild(li);
 				music.push(li);
@@ -189,13 +235,38 @@ function populateList(arr,e,dir){
 		}
 	}
 }
+function showError(msg){
+	if(!msg){
+		if(!!audio.style.display){
+			audio.removeAttribute('style');
+		}
+		if(!!err.className)
+			err.removeAttribute('class');
+	}
+	else{
+		console.error(msg);
+		audio.style.display='none';
+		err.className="open";
+		err.textContent=msg;
+	}
+}
 function init(){
+	log=log?console.log:function(){};
 	log('Begin Main JavaScript File');
 	var config=localStorage.getItem("HTML5_music"),
 		ul=document.createElement('ul');
 	audio=getId('audio');
-	title=getId('title');
-	TITLE=getId('pageTitle');
+	title={
+		"player":getId('title'),
+		"head":getId('pageTitle'),
+	}
+	title.page=title.head.textContent;
+	ID3={
+		"title":getId('id3_title'),
+		"artist":getId('id3_artist'),
+		"album":getId('id3_album'),
+		"year":getId('id3_year')
+	};
 	pic=getId('cover');
 	shuffle=getId('shuffle');
 	loop=getId('loop');
@@ -205,7 +276,10 @@ function init(){
 	playlist=getId('playlist');
 	playlist.appendChild(ul);
 	sendEvt(window,'resize');
-	populateList(library['music'],ul,library['path']);
+	populateList(library.music,ul,library.path);
+	if(!library.id3){
+		getId('id3').style.display='none';
+	}
 	if(config==null){
 		log('Reset config');
 		config=null;
@@ -221,8 +295,7 @@ function init(){
 		"tracks":0,
 		"unPlayed":[]
 	};
-	log('Config Data:');
-	log(config);
+	log('Config Data:',config);
 	unPlayed=config.unPlayed;
 	if(unPlayed.length==0||config.tracks!=music.length){
 		reloadUnPlayed(true);
@@ -236,7 +309,7 @@ function init(){
 		}
 	}
 	audio.addEventListener("ended",function(){
-		log('End Track: #'+track);
+		log('End Track:',track);
 		var next=track,
 			indx=setPlayed(track,true);
 		if(loop.checked){
@@ -247,7 +320,7 @@ function init(){
 		hst.nav=false;
 		hst.indx++;
 		if(hst.log.length>hst.indx){
-			log('Back feature was used: '+hst.indx+'/'+hst.log.length);
+			log('Back feature was used:',hst.indx,'of',hst.log.length);
 			sendEvt(music[hst.log[hst.indx]],'click');
 			return true;
 		}
@@ -282,6 +355,7 @@ function init(){
 			audio.currentTime=0;
 			return audio.play();
 		}
+		log("Next Track ID is: #",next);
 		sendEvt(music[next],'click');
 	},false);
 	getId('next').addEventListener("click",function(){
@@ -289,7 +363,7 @@ function init(){
 	},false);
 	getId('back').addEventListener("click",function(){
 		if(hst.indx==0){
-			log('History: '+hst.indx+' - '+JSON.stringify(hst.log));
+			log('History:',hst.indx,'-',hst.log);
 			return alert('Out of History');
 		}
 		if(hst.indx==hst.log.length){
@@ -298,7 +372,7 @@ function init(){
 			}
 		}
 		hst.indx--;
-		log('History: '+hst.indx+' - '+JSON.stringify(hst.log));
+		log('History:',hst.indx,'-',hst.log);
 		hst.nav=false;
 		sendEvt(music[hst.log[hst.indx]],'click');
 	},false);
@@ -310,9 +384,9 @@ function init(){
 			reloadUnPlayed(false);
 		}
 	},false);
-	shuffle.checked=config["shuffle"];
-	loop.checked=config["loop"];
-	repeat.checked=config["repeat"];
+	shuffle.checked=config.shuffle;
+	loop.checked=config.loop;
+	repeat.checked=config.repeat;
 	repeat.parentNode.title="Played: "+(music.length-unPlayed.length)+"/"+music.length;
 	if(loop.checked){
 		repeat.disabled=true;
@@ -322,21 +396,21 @@ function init(){
 		repeat.disabled=this.checked;
 		shuffle.disabled=this.checked;
 	},false);
-	track=config["track"]>music.length-1?0:config["track"];
-	if(config['time']==0&&track==0){
+	track=config.track>music.length-1?0:config.track;
+	if(config.time==0&&track==0){
 		sendEvt(audio,'ended');
 	}
 	else{
 		sendEvt(music[track],'click');
 		audio.pause();
 		wait4It(function(){
-			audio.currentTime=config["time"];
-			if(config["state"]===false){
+			audio.currentTime=config.time;
+			if(config.state===false){
 				audio.play();
 			}
 		});
 	}
-	audio.volume=config["volume"];
+	audio.volume=config.volume;
 	window.onunload=function(){
 		if( isNaN(track) || isNaN(audio.currentTime) || isNaN(audio.volume) ){
 			log('Abort save');
@@ -384,7 +458,7 @@ function init(){
 			}
 			setTimeout(function(){hst.debugHst(e);},100);
 		}
-		cLog=true;
+		log=console.log;
 		hst.debugHst(hst.debug);
 		playlist.setAttribute('style','max-height:60px;min-height:30px;');// Let me see what I'm doing
 	}
--- playlist.php
+++ playlist.php
@@ -1,19 +1,49 @@
-<?php 
+<?php
 $F='library';// Name of folder containing music
-header("content-type: application/javascript"); 
+$getID3='/usr/share/php/getid3/getid3.php';// Path to php getID3; Use non-existant file path to disable
+$initGetID3=false;// Send ID3 data on page load *** DO NOT USE THIS ON SLOW SERVER LIKE A RASPBERRY PI ***
+/* ---- PHP getID3 Notes -----
+	* php-getid3 is not required
+	Set $getID3 to the /path/to/getid3.php
+	The php-getid3 package will install it to: /usr/share/php/getid3/getid3.php
+
+	When initGetID3 is set to true ALL id3 data will be sent at page load, this takes over 100 times longer
+	When initGetID3 is set to false the client will ask the server for id3 data if php-getid3 is installed
+*/
+if(isset($_GET['file'])&&isset($_GET['track'])){
+	header("content-type: application/json; charset=utf-8");
+	$file=explode('/',$_GET['file']);
+	$track=(int)$_GET['track'];
+	if(!file_exists($getID3)||array_search('..',$file)!==false||$file[0]!=$F){// Change in server config or theoretical attack on server
+		die('{"id3":false,"track":'.$track.',"access":"denied"}');
+	}
+	$file=$_GET['file'];
+	if(!file_exists($file)){
+		header("HTTP/1.0 404 Not Found");
+		die('{"id3":false,"track":'.$track.'}');
+	}
+	require_once($getID3);
+	$getID3=new getID3;
+	$getID3=$getID3->analyze($file)['tags']['id3v2'];
+	die(json_encode((object)array(
+		"id3"=>$getID3===null?false:$getID3,
+		"track"=>$track
+	)));
+}
+header("content-type: application/javascript; charset=utf-8");
 ?>
 var library=<?php
-	function tree($dir,$depth){
+	function tree($dir,$depth,$getID3){
 		$json=new stdClass();
 		$scan=scandir($dir);
 		$files=array();
 		foreach($scan as $f){
-			if(substr($f,0,1)=="."){// Skip hidden files (UNIX defination)
+			if(substr($f,0,1)=="."||substr($f,-4)=='.txt'){// Skip text files and hidden files (UNIX defination)
 				continue;
 			}
 			if(is_dir("$dir/$f")){
 				if($depth<10){// Max folder depth, this is to prevent a infinte loop
-					$json->{$f}=tree("$dir/$f",$depth+1);
+					$json->{$f}=tree("$dir/$f",$depth+1,$getID3);
 				}
 				else{
 					$json->{$f}=array("Error: Too Deep"=>array("Infinite loop prevention"=>array()));
@@ -24,9 +54,46 @@ var library=<?php
 			}
 		}
 		if(count($files)>0){
-			$json->{"/"}=$files;
+			$mfiles=(object)array();
+			foreach($files as $f){
+				if(!$getID3){
+					$mfiles->{$f}=false;
+				}
+				else if(strtolower(substr($f,0,-3))=='cover.'){
+					$mfiles->{$f}=false;
+				}
+				else{
+					$mfiles->{$f}=$getID3->analyze("$dir/$f")['tags']['id3v2'];
+				}
+			}
+			$json->{"/"}=$mfiles;
 		}
 		return $json;
 	}
-	echo json_encode(array('music'=>tree($F,0),'path'=>"$F/"));
-?>
+	$mtime=microtime(true);
+	if($initGetID3==true && file_exists($getID3)){
+		require_once($getID3);
+		$getID3=new getID3;
+	}
+	else{
+		$initGetID3=file_exists($getID3);
+		$getID3=false;
+	}
+	echo json_encode(array(
+		'music'=>tree($F,0,$getID3),
+		'path'=>"$F/",
+		'id3'=>$initGetID3||!is_bool($getID3),
+		'loadTime'=>microtime(true)-$mtime
+	));
+?>;
+if(console&&console.log){
+	console.log("Playlist load time: "+library.loadTime);
+}
+else{
+	var console={
+		"log":function(){},
+		"info":function(){},
+		"warn":function(){},
+		"error":function(){}
+	};
+}
